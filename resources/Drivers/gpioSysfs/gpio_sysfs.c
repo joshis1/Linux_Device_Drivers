7dcb094cf2b58b301e371d621621a815955e1740
Shreyas Joshi <shreyas.joshi@biamp.com>
2020-11-16 22:07:58 -0800

    device create and sysfs completed

--- resources/Drivers/gpioSysfs/gpio_sysfs.c
+++ resources/Drivers/gpioSysfs/gpio_sysfs.c
@@ -15,6 +15,8 @@
 /**platform_device_id - match based out of platform_device_id **/
 #include <linux/mod_devicetable.h>
 #include <linux/of_device.h>
+/** for accessing gpios **/
+#include <linux/gpio/consumer.h>
 
 
 /**private static prototypes**/
@@ -24,6 +26,7 @@
 struct gpiodev_private_data
 {
    char label[20];
+   struct gpio_desc *desc;
 };
 
 struct gpiodrv_private_data 
@@ -33,6 +36,8 @@ struct gpiodrv_private_data
 };
 
 struct gpiodrv_private_data gpiodrv_data;
+
+
 
 /**Trick to get the function name **/
 #undef pr_fmt
@@ -44,8 +49,109 @@ struct of_device_id gpio_sysfs_dt_match[] = {
 };
 
 
+ssize_t direction_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+   return 0;
+}
+
+ssize_t direction_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+   return 0;
+}
+
+ssize_t value_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+   return 0;
+}
+
+ssize_t value_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+   return 0;
+}
+
+ssize_t label_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+   return 0;
+}
+
+static DEVICE_ATTR_RW(direction);
+static DEVICE_ATTR_RW(value);
+static DEVICE_ATTR_RO(label);
+
+
+static struct attribute	*gpio_attrs[] = {
+   &dev_attr_direction.attr,
+   &dev_attr_value.attr,
+   &dev_attr_label.attr,
+   NULL
+};
+
+static struct attribute_group gpio_group = {
+   .attrs = gpio_attrs
+};
+
+const struct attribute_group *gpio_attr_groups[] =
+{
+   &gpio_group,
+   NULL
+};
+
+
 int gpio_sysfs_platform_driver_probe (struct platform_device *pDev)
 {   
+   struct device_node *parent = pDev->dev.of_node;
+   struct device_node *child;
+   struct gpiodev_private_data *dev_data;
+
+   struct device	dev = pDev->dev;
+   const char *name;
+   int i = 0;
+   int ret;
+   struct device *dev_sysfs;
+
+   for_each_available_child_of_node(parent, child)
+   {
+      dev_data = devm_kzalloc(&dev, sizeof(struct gpiodev_private_data),GFP_KERNEL);
+      if(!dev_data)
+      {
+         pr_err("No memory \r\n");
+         return -ENOMEM;
+      }
+      if(of_property_read_string(child, "label", &name))
+      {
+         pr_err("Missing label \r\n");
+         snprintf(dev_data->label,sizeof(dev_data->label), "unknown-gpio-%d",i);
+      }
+      else
+      {
+         strcpy(dev_data->label, name);
+         dev_info(&dev,"GPIO label = %s\n",dev_data->label);
+      }
+      i++;
+      dev_data->desc = devm_fwnode_get_gpiod_from_child(&dev,"bone",&child->fwnode, GPIOD_ASIS, dev_data->label);
+      if(IS_ERR(dev_data->desc)) {
+         ret = PTR_ERR(dev_data->desc);
+         if(ret == -ENONET) {
+            dev_err(&dev, "get gpiod failed - no entry found\r\n");
+         }
+         return ret;
+      }
+      /** gpio/gpio.txt **/
+      ret = gpiod_direction_output(dev_data->desc, 0);
+      if(ret) {
+         dev_err(&dev,"Err setting gpio direction failed = %d\r\n",i);
+         return ret;
+      }
+      
+      dev_sysfs = device_create_with_groups(gpiodrv_data.class_gpio, &dev, 0, dev_data, gpio_attr_groups, dev_data->label);
+      if(IS_ERR(dev_sysfs)) {
+         ret = PTR_ERR(dev_sysfs);
+         dev_err(&dev, "Error creating device with groups \r\n");
+         return ret;
+      }
+
+   }
+
    return 0;
 }
 
@@ -80,6 +186,10 @@ static void __exit gpio_sysfs_exit(void)
    class_destroy(gpiodrv_data.class_gpio);
    pr_info("module unloaded\n");
 }
+
+
+
+
 
 /**module registration **/
 module_init(gpio_sysfs_init);
